c5f1139d050a7ce3ba3883272fe7a1002798fcec
Camille <camilleguillenf.almeida@gmail.com>
2024-09-09 21:18:15 -0300

    atualização algoritmos

--- Atividade B3-3/Algoritmos/Códigos/busca_binaria.c
+++ Atividade B3-3/Algoritmos/Códigos/busca_binaria.c
@@ -12,30 +12,33 @@
 
 int binary_search(int arr[], int size, int target) {
     int left = 0;
-    int right = size - 1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == target) {
-            return mid;  // Encontrado
+    int right = size - 1; // 2t
+    while (left <= right) { // t (log n)  
+        int mid = left + (right - left) / 2; // 4t (log n)
+        if (arr[mid] == target) { // 2t (log n)
+            return mid;  // Encontrado  // t
         }
-        if (arr[mid] < target) {
-            left = mid + 1;
+        if (arr[mid] < target) { //2t (log n)
+            left = mid + 1; //2t (log n)
         } else {
-            right = mid - 1;
+            right = mid - 1; //2t (log n)
         }
     }
-    return -1;  // Não encontrado
+    return -1;  // Não encontrado // t 
 }
 
+
 int main() {
-    int arr[] = {1, 2, 3, 4, 5};
+    int arr[] = {1, 2, 3, 4, 5}; // t 
     int size = sizeof(arr) / sizeof(arr[0]);
-    int target = 4;
+    int target = 4; // t
     int result = binary_search(arr, size, target);
     if (result != -1) {
-        printf("Elemento encontrado no índice %d\n", result);
+        printf("Elemento encontrado no índice %d\n", result); // t + algoritmo 
     } else {
-        printf("Elemento não encontrado\n");
+        printf("Elemento não encontrado\n"); // t 
     }
     return 0;
 }
+
+// Busca Binária - 11t(log₂(n)) + 8t
--- Atividade B3-3/Algoritmos/Códigos/busca_linear_em_ordem.c
+++ Atividade B3-3/Algoritmos/Códigos/busca_linear_em_ordem.c
@@ -11,26 +11,28 @@
 #include <stdio.h>
 
 int ordered_linear_search(int arr[], int size, int target) {
-    for (int i = 0; i < size; i++) {
-        if (arr[i] == target) {
-            return i;  // Encontrado
+    for (int i = 0; i < size; i++) { // 4tn 
+        if (arr[i] == target) { // 2tn
+            return i;  // Encontrado  // t
         }
         if (arr[i] > target) {
             break;  // A lista está ordenada, pode parar se passar o alvo
         }
     }
-    return -1;  // Não encontrado
+    return -1;  // Não encontrado  // t
 }
 
 int main() {
-    int arr[] = {1, 2, 3, 4, 5};
+    int arr[] = {1, 2, 3, 4, 5}; // t
     int size = sizeof(arr) / sizeof(arr[0]);
-    int target = 4;
+    int target = 4; // t
     int result = ordered_linear_search(arr, size, target);
     if (result != -1) {
-        printf("Elemento encontrado no índice %d\n", result);
+        printf("Elemento encontrado no índice %d\n", result); // t + algoritmo
     } else {
-        printf("Elemento não encontrado\n");
+        printf("Elemento não encontrado\n"); // t
     }
     return 0;
 }
+
+// Busca Linear em Ordem - 7tn + 9t 
